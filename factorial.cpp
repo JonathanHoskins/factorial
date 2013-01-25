3dd2821c73a987ecf9d8a66ad51c0b529295015a
Jonathan Hoskins <jonison06@gmail.com>
2013-01-25 11:27:58 -0500

    Update factorial.cpp
    Added "long long" in front of factorialNumber when declaring it to allow for 20!.

--- factorial.cpp
+++ factorial.cpp
@@ -11,35 +11,42 @@ Reference material - http://www.mycoding.net/2011/02/c-program-to-find-the-facto
 #include <istream>
 
 using namespace std;
-long input;
-long factorialNumber = 1;
+int input;
+//Adding "long long" in front of int factorialNumber to allow for 20!.
+long long int factorialNumber = 1;
 
 //Method for finding the factorial of a number
-long Factorial(long input)
+int Factorial(int input)
 {
-	//For loop runs until it reaches the number of cycles equal to input
-	//While running, the loop multiplies the factorialNumber by i, then assigns the value to itself(factorialNumber)
-	//When the loop reaches the end, it returns the desired factorial
-	for(int i = 1; i <= input; i++)
-		factorialNumber = factorialNumber * i;
-		cout << "The factorial of " << input << " is: ";
-		return factorialNumber;
+//For loop runs until it reaches the number of cycles equal to input
+//While running, the loop multiplies the factorialNumber by i, then assigns the value to itself(factorialNumber)
+//When the loop reaches the end, it returns the desired factorial
+for(int i = 1; i <= input; i++)
+	factorialNumber *= i;
+	cout << "The factorial of " << input << " is: ";
+	return factorialNumber;
 }
 
-long main()
+int main()
 {
 	cout << "Please input a number to find its factorial: ";
 	cin >> input;
 	//This instance makes sure that the user inputs a positive integer number
 	if(input < 0){
-		cout << "You entered a negative number!\n";
+	cout << "You entered a negative number!\n";
 	}
-	//If the user enters a negative number, this is printed out	
+	//If the user enters a negative number, this is printed out
 	else{
-		Factorial(input);
-		cout << factorialNumber << "\n";
-	}
-	
-	return 0;
+	Factorial(input);
+	cout << factorialNumber << "\n";
 }
+	//I put this in so that I could output the numbers to a file for easy checking.
+	//I did this because20! was giving me a negative number.
+	//I had to add "long long" in front of int factorialNumber when I declared it.
+	ofstream myfile;
+	myfile.open ("outputFromFactorial.txt");
+	myfile << "Input Number: " << input << "\n" << "Factorial Number: " << factorialNumber << "\n";
+	myfile.close();
 
+return 0;
+}
